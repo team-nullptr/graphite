6bdd0d4b036333b7b89676721e077205c8f43b2e
Bartosz Bieniek <itsbk20@gmail.com>
2023-04-16 14:36:56 +0200

    Add support for multiple edges between two points

--- src/simulator/components/Edge.tsx
+++ src/simulator/components/Edge.tsx
@@ -1,6 +1,7 @@
+import { Position } from "../Simulator";
 import styles from "./Edge.module.css";
 
-const vertexRadius = 19;
+const vertexRadius = 20;
 
 export interface EdgeProps {
   x: number;
@@ -8,35 +9,77 @@ export interface EdgeProps {
   dx: number;
   dy: number;
   directed?: boolean;
+  position?: number;
 }
 
 export const Edge = (props: EdgeProps) => {
   const width = props.dx - props.x;
   const height = props.dy - props.y;
 
-  const rad = Math.atan2(height, width);
-  const deg = (rad * 180) / Math.PI + 180;
-
+  const angle = (props.position ?? 0) * 0.35;
   const length = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
 
-  const offsetX = (vertexRadius * width) / length;
-  const offsetY = (vertexRadius * height) / length;
+  const start = rotate([vertexRadius, 0], angle);
+  const end = rotate([length - vertexRadius, 0], -angle, [length, 0]);
 
-  const path = props.directed
-    ? getArrowPath(length - 2 * vertexRadius)
-    : getLinePath(length - 2 * vertexRadius);
-
+  const linePath = getLinePath(start, end, angle);
   // prettier-ignore
-  const transform = `translate(${props.dx - offsetX} ${props.dy - offsetY}) rotate(${deg})`;
+  const arrowPath = getArrowPath([length - vertexRadius, 0], [length, 0], -angle, true);
 
-  return <path className={styles.edge} d={path} transform={transform} />;
+  const rad = Math.atan2(height, width);
+  const deg = (rad * 180) / Math.PI;
+
+  const transform = `translate(${props.x} ${props.y}) rotate(${deg})`;
+
+  return (
+    <path
+      className={styles.edge}
+      // prettier-ignore
+      d={linePath + (props.directed ? arrowPath : "")}
+      transform={transform}
+    />
+  );
 };
 
-const getArrowPath = (length: number): string => {
-  // prettier-ignore
-  return `M6.02565 6.05129L0 0.0256458L6.02565 -6L6.62021 -5.41025L1.60098 -0.391021H${length}V0.442312H1.60098L6.62021 5.46154L6.02565 6.05129Z`;
+const getArrowPath = (
+  start: Position,
+  origin: Position,
+  angle: number,
+  reversed: boolean = false
+): string => {
+  const deltaX = reversed ? -6 : 6;
+
+  const [sx, sy] = start;
+  const [ax, ay] = rotate([sx + deltaX, sy - 6], angle, origin);
+  const [bx, by] = rotate([sx + deltaX, sy + 6], angle, origin);
+  const [mx, my] = rotate([sx, sy], angle, origin);
+
+  return `M${ax} ${ay}L${mx} ${my}L${bx} ${by}`;
 };
 
-const getLinePath = (length: number): string => {
-  return `M0 -0.391021H${length}V0.442312H0Z`;
+const getLinePath = (a: Position, b: Position, angle: number): string => {
+  const [ax, ay] = a;
+  const [bx, by] = b;
+
+  const hx = (ax + bx) / 2;
+  const hy = Math.tan(angle) * hx;
+
+  return `M${ax} ${ay}Q${hx} ${hy} ${bx} ${by}`;
+};
+
+const rotate = (
+  point: Position,
+  angle: number,
+  origin: Position = [0, 0]
+): Position => {
+  const sin = Math.sin(angle);
+  const cos = Math.cos(angle);
+
+  let [px, py] = point;
+  let [ox, oy] = origin;
+
+  const x = (px - ox) * cos - (py - oy) * sin;
+  const y = (px - ox) * sin + (py - oy) * cos;
+
+  return [x + ox, y + oy];
 };
