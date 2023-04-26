f8015958b3d7f985a5195578a32715784d37d8db
Bartosz Bieniek <itsbk20@gmail.com>
2023-04-26 21:46:01 +0200

    Implement rearranging vertices

--- src/simulator/Simulator.tsx
+++ src/simulator/Simulator.tsx
@@ -1,20 +1,35 @@
-import { useMemo } from "react";
-import { Graph } from "../engine/graph";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Graph, Vertex as IVertex } from "../engine/graph";
 import styles from "./Simulator.module.css";
 import { Edge } from "./components/Edge";
 import { Vertex } from "./components/Vertex";
-import { distributeEdges, groupEdges, sortEdges } from "./util/distributeEdges";
 import { Position } from "./model/position";
+import {
+  PositionedEdge,
+  distributeEdges,
+  groupEdges,
+  sortEdges,
+} from "./util/distributeEdges";
 
 export type Arrangement = { [key: string]: Position };
 
 export interface SimulatorProps {
   graph: Graph;
-  arrangement: Arrangement;
+  onRearrange?: (newArrangement: Arrangement) => void;
+  arrangement?: Arrangement;
+}
+
+interface SelectedVertex {
+  id: string;
+  offset: Position;
 }
 
 export const Simulator = (props: SimulatorProps) => {
-  const { graph, arrangement } = props;
+  const initialArrangement = props.arrangement ?? {};
+  const [arrangement, setArrangement] = useState(initialArrangement);
+  const selectedVertex = useRef<SelectedVertex>();
+  const svgRef = useRef<SVGSVGElement>(null);
+  const { graph } = props;
 
   const positionedEdges = useMemo(() => {
     const connections = groupEdges(graph.edges);
@@ -25,42 +40,104 @@ export const Simulator = (props: SimulatorProps) => {
     });
   }, [graph]);
 
-  const renderEdges = () =>
-    positionedEdges.flatMap((group) => {
-      return group.map((positionedEdge) => {
-        const [edge, position] = positionedEdge;
-        const [x, y] = arrangement[edge.a.id] ?? [0, 0];
-        const [dx, dy] = arrangement[edge.b.id] ?? [0, 0];
-        const circular = edge.a.id === edge.b.id;
+  useEffect(() => {
+    const mouseMoveHandler = (event: MouseEvent) => {
+      const boundingBox = svgRef.current?.getBoundingClientRect();
+      if (!boundingBox) return;
+      const vertex = selectedVertex.current;
+      if (!vertex) return;
 
-        return (
-          <Edge
-            key={edge.id}
-            position={position}
-            x={x}
-            y={y}
-            dx={dx}
-            dy={dy}
-            directed={edge.directed}
-            circular={circular}
-          />
-        );
+      const [ox, oy] = vertex.offset;
+      const x = event.clientX - boundingBox.left - ox;
+      const y = event.clientY - boundingBox.top - oy;
+
+      setArrangement((arrangement) => {
+        return { ...arrangement, [vertex.id]: [x, y] };
       });
-    });
+    };
 
-  const renderVertices = () =>
-    graph.vertices.map((vertex) => {
-      const [x, y] = arrangement[vertex.id] ?? [0, 0];
+    const mouseUpHandler = () => {
+      selectedVertex.current = undefined;
+    };
 
-      return (
-        <Vertex key={vertex.id} cx={x} cy={y} value={vertex.value as any} />
-      );
-    });
+    addEventListener("mousemove", mouseMoveHandler);
+    addEventListener("mouseup", mouseUpHandler);
+
+    return () => {
+      removeEventListener("mousemove", mouseMoveHandler);
+      removeEventListener("mouseup", mouseUpHandler);
+    };
+  }, []);
+
+  const vertexMouseDownHandler = (vertex: IVertex, offset: Position) => {
+    selectedVertex.current = { id: vertex.id, offset };
+  };
 
   return (
-    <svg className={styles.simulator}>
-      {renderEdges()}
-      {renderVertices()}
+    <svg ref={svgRef} className={styles.simulator}>
+      {/* prettier-ignore */}
+      <Edges
+        edges={positionedEdges.flat()}
+        arrangement={arrangement}
+      />
+      <Vertices
+        vertices={graph.vertices}
+        arrangement={arrangement}
+        onMouseDown={vertexMouseDownHandler}
+      />
     </svg>
   );
+};
+
+interface VerticesProps {
+  vertices: IVertex[];
+  arrangement: Arrangement;
+  onMouseDown: (vertex: IVertex, offset: Position) => void;
+}
+
+const Vertices = (props: VerticesProps) => {
+  const renderedVertices = props.vertices.map((vertex) => {
+    const [x, y] = props.arrangement[vertex.id] ?? [0, 0];
+    const { id, value } = vertex;
+    return (
+      <Vertex
+        key={id}
+        cx={x}
+        cy={y}
+        value={value as any}
+        onMouseDown={(offset) => props.onMouseDown(vertex, offset)}
+      />
+    );
+  });
+
+  return <>{renderedVertices}</>;
+};
+
+interface EdgesProps {
+  edges: PositionedEdge[];
+  arrangement: Arrangement;
+}
+
+const Edges = (props: EdgesProps) => {
+  const renderedEdges = props.edges.map((positionedEdge) => {
+    const [edge, position] = positionedEdge;
+    const [x, y] = props.arrangement[edge.a.id] ?? [0, 0];
+    const [dx, dy] = props.arrangement[edge.b.id] ?? [0, 0];
+    const circular = edge.a.id === edge.b.id;
+
+    return (
+      <Edge
+        key={edge.id}
+        position={position}
+        x={x}
+        y={y}
+        dx={dx}
+        dy={dy}
+        directed={edge.directed}
+        circular={circular}
+      />
+    );
+  });
+
+  return <>{renderedEdges}</>;
 };
--- src/simulator/components/Vertex.tsx
+++ src/simulator/components/Vertex.tsx
@@ -1,14 +1,30 @@
+import { MouseEvent, useRef } from "react";
+import { Position } from "../model/position";
 import styles from "./Vertex.module.css";
 
 export interface VertexProps {
   cx: number;
   cy: number;
   value: string;
+  onMouseDown?: (offset: Position) => void;
 }
 
 export const Vertex = (props: VertexProps) => {
+  const ref = useRef<SVGGElement>(null);
+
+  const mouseDownHandler = (event: MouseEvent) => {
+    const boundingBox = ref.current?.getBoundingClientRect();
+    if (!boundingBox) return;
+
+    const offsetX = event.pageX - boundingBox.left - 19;
+    const offsetY = event.pageY - boundingBox.top - 19;
+
+    const offset: Position = [offsetX, offsetY];
+    props.onMouseDown?.(offset);
+  };
+
   return (
-    <g className={styles.vertex}>
+    <g ref={ref} className={styles.vertex} onMouseDown={mouseDownHandler}>
       <circle cx={props.cx} cy={props.cy} r={19} />
       <text x={props.cx} y={props.cy}>
         {props.value}
