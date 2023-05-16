8d83eb937edad802febf1c112bab8ccc9b702f41
Bartosz Bieniek <itsbk20@gmail.com>
2023-05-16 22:19:39 +0200

    Install react-router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { Editor } from "./features/editor/Editor";
-
-export const App = () => {
-  return <Editor stepCount={7}></Editor>;
-};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Editor } from "./features/editor/Editor";
 import "./index.css";
-import { App } from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "projects/:projectId",
+    element: <Editor />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
