577690005d2831a322d1e80723e0287015e6669b
senicko <sebastianflajszer@gmail.com>
2023-06-12 08:18:06 +0200

    Revert "Make a demo of force-directed auto layout"
    This reverts commit ea8e190c71583c8a34c1b4d5dec44956043be8b8.

--- src/features/graph-view/GraphView.tsx
+++ src/features/graph-view/GraphView.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { Edge } from "./components/Edge";
 import { Vertex } from "./components/Vertex";
+import { useArrangement } from "./hooks/useArrangement";
 import { distributeEdges, groupEdges, sortEdges } from "./util/distributeEdges";
 import { useEditorStore } from "../editor/context/editor";
 import { Highlights } from "../../engine/runner/instruction";
-import { forceArrange, randomArrange } from "./force-arrangement";
-import { Vec2d } from "./vec2d";
-
-type Arrangement = Record<string, Vec2d>;
 
 export type GraphViewProps = {
   highlights?: Highlights;
@@ -15,30 +12,7 @@ export type GraphViewProps = {
 
 export const GraphView = (props: GraphViewProps) => {
   const graph = useEditorStore((state) => state.graph);
-
-  const [arrangement, setArrangement] = useState<Arrangement>(
-    randomArrange(graph)
-  );
-
-  useEffect(() => {
-    window.addEventListener("keyup", (e) => {
-      if (e.code === "Backquote") {
-        setArrangement(randomArrange(graph));
-      }
-    });
-  }, []);
-
-  const update = () => {
-    setArrangement((a) => forceArrange(graph, { ...a }));
-  };
-
-  useEffect(() => {
-    setArrangement(randomArrange(graph));
-
-    const id = setInterval(() => update(), 10);
-
-    return () => clearInterval(id);
-  }, [graph]);
+  const { arrangement, vertexMouseDownHandler, svgRef } = useArrangement();
 
   const positionedEdges = useMemo(() => {
     const connections = groupEdges(Object.values(graph.edges));
@@ -50,54 +24,52 @@ export const GraphView = (props: GraphViewProps) => {
     });
   }, [graph]);
 
-  const view = useMemo(() => {
-    return (
-      <>
-        {Object.values(graph.vertices).map((vertex) => {
-          const { x, y } = arrangement[vertex.id] ?? new Vec2d([0, 0]);
-          const { id } = vertex;
-          const hue = props.highlights?.get(id);
+  const renderVertices = () =>
+    Object.values(graph.vertices).map((vertex) => {
+      const [x, y] = arrangement[vertex.id] ?? [0, 0];
+      const { id } = vertex;
+      const hue = props.highlights?.get(id);
 
-          return (
-            <Vertex
-              hue={hue}
-              key={id}
-              cx={x * 5}
-              cy={y * 5}
-              value={id}
-              onMouseDown={(offset) => console.log(offset)}
-            />
-          );
-        })}
-        {positionedEdges.flat().map((positionedEdge) => {
-          const [edge, position] = positionedEdge;
-          const { x, y } = arrangement[edge.from] ?? new Vec2d([0, 0]);
-          const { x: dx, y: dy } = arrangement[edge.to] ?? new Vec2d([0, 0]);
-          const circular = edge.from === edge.to;
+      return (
+        <Vertex
+          hue={hue}
+          key={id}
+          cx={x}
+          cy={y}
+          value={id}
+          onMouseDown={(offset) => vertexMouseDownHandler(vertex, offset)}
+        />
+      );
+    });
 
-          return (
-            <Edge
-              key={edge.id}
-              position={position}
-              x={x * 5}
-              y={y * 5}
-              dx={dx * 5}
-              dy={dy * 5}
-              directed={edge.directed}
-              circular={circular}
-            />
-          );
-        })}
-      </>
-    );
-  }, [arrangement]);
+  const renderEdges = () =>
+    positionedEdges.flat().map((positionedEdge) => {
+      const [edge, position] = positionedEdge;
+      const [x, y] = arrangement[edge.from] ?? [0, 0];
+      const [dx, dy] = arrangement[edge.to] ?? [0, 0];
+      const circular = edge.from === edge.to;
+
+      return (
+        <Edge
+          key={edge.id}
+          position={position}
+          x={x}
+          y={y}
+          dx={dx}
+          dy={dy}
+          directed={edge.directed}
+          circular={circular}
+        />
+      );
+    });
 
   return (
     <svg
-      // ref={svgRef}
+      ref={svgRef}
       className="h-full w-full bg-base-200 dark:bg-base-300-dark"
     >
-      {view}
+      {renderEdges()}
+      {renderVertices()}
     </svg>
   );
 };
