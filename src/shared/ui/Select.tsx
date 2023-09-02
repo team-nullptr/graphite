0f0b66405035f30595280fde04c206f3d4e3b361
LordPrinz <plazinski2902@gmail.com>
2023-09-02 16:37:37 +0200

    Create algorithm vertex picker with RadixUI

--- src/shared/ui/Select.tsx
+++ src/shared/ui/Select.tsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon, ChevronUpIcon, ArrowUpRightIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon, ChevronUpIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 import * as RadixSelect from "@radix-ui/react-select";
 
@@ -19,18 +19,19 @@ export function Select({ values, label, value, onChange }: SelectOptions) {
       onValueChange={(selectedValue) => {
         onChange(selectedValue);
       }}
+      value={value}
     >
       <RadixSelect.Trigger
-        aria-label="Vertex picker"
-        className="relative w-full rounded-md border px-4 py-2 text-left"
+        aria-label={label}
+        className="relative w-full rounded-md border  px-4 py-2 text-left text-sm outline-none "
       >
-        <RadixSelect.Value placeholder="Choose starting vertex" />
+        <RadixSelect.Value placeholder={label} />
         <RadixSelect.Icon className="absolute bottom-0 right-2 top-0 my-auto h-4 w-4">
           <ChevronDownIcon />
         </RadixSelect.Icon>
       </RadixSelect.Trigger>
       <RadixSelect.Portal>
-        <RadixSelect.Content>
+        <RadixSelect.Content className="w-full overflow-hidden rounded border border-slate-200 bg-slate-50">
           <RadixSelect.ScrollDownButton>
             <ChevronUpIcon />
           </RadixSelect.ScrollDownButton>
@@ -39,7 +40,7 @@ export function Select({ values, label, value, onChange }: SelectOptions) {
               <SelectItem key={val} value={val} />
             ))}
           </RadixSelect.Viewport>
-          <RadixSelect.ScrollDownButton className="text-violet11 flex h-[25px] cursor-default items-center justify-center bg-white">
+          <RadixSelect.ScrollDownButton className="flex h-6 cursor-default items-center justify-center bg-white">
             <ChevronDownIcon />
           </RadixSelect.ScrollDownButton>
         </RadixSelect.Content>
@@ -50,10 +51,15 @@ export function Select({ values, label, value, onChange }: SelectOptions) {
 
 const SelectItem = ({ value }: SelectItemOptions) => {
   return (
-    <RadixSelect.Item value={value}>
-      <RadixSelect.ItemText className="text-left">{value}</RadixSelect.ItemText>
-      <RadixSelect.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
-        <ArrowUpRightIcon />
+    <RadixSelect.Item
+      value={value}
+      className="relative flex h-9 select-none items-center rounded-sm pl-6 pr-8 text-sm leading-none data-[disabled]:pointer-events-none data-[highlighted]:bg-slate-100 data-[highlighted]:outline-none"
+    >
+      <RadixSelect.ItemText className="translate-x-3 text-left">
+        Vertex {value}
+      </RadixSelect.ItemText>
+      <RadixSelect.ItemIndicator className="absolute right-3 inline-flex w-4 items-center justify-center">
+        <CheckIcon />
       </RadixSelect.ItemIndicator>
     </RadixSelect.Item>
   );
