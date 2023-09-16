18b719943711180427b10a49a01a4ac6e8445a6f
LordPrinz <plazinski2902@gmail.com>
2023-09-16 22:54:13 +0200

    Create horizontal scroll hook

---
+++ src/shared/hooks/useHorizontalScroll.ts
@@ -0,0 +1,21 @@
+import { useRef, useEffect, RefObject } from "react";
+
+export function useHorizontalScroll() {
+  const elRef: RefObject<HTMLDivElement> = useRef(null);
+  useEffect(() => {
+    const el = elRef.current;
+    if (el) {
+      const onWheel = (e: WheelEvent) => {
+        if (e.deltaY == 0) return;
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft + e.deltaY,
+          behavior: "smooth",
+        });
+      };
+      el.addEventListener("wheel", onWheel);
+      return () => el.removeEventListener("wheel", onWheel);
+    }
+  }, []);
+  return elRef;
+}
