0652194584143798ca36b340e32b9a76bfe309ff
LordPrinz <plazinski2902@gmail.com>
2023-09-16 22:57:25 +0200

    Implement Horizontal scroll

--- src/pages/editor/components/ArrayStep.tsx
+++ src/pages/editor/components/ArrayStep.tsx
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { ArrayState } from "~/core/simulator/state";
 import { cn } from "~/lib/utils";
+import { useHorizontalScroll } from "~/shared/hooks/useHorizontalScroll";
 
 export type ArrayStepProps = {
   state: ArrayState;
@@ -8,12 +9,17 @@ export type ArrayStepProps = {
 };
 
 export function ArrayStep({ state, visibleCells = 8 }: ArrayStepProps) {
+  const scrollRef = useHorizontalScroll();
+
   return (
     <div className="space-y-4 p-4">
       <span className="font-md text-slate-800">{state.title}</span>
       <div className="flex items-center gap-2">
         <ChevronLeftIcon className="w-8 min-w-[28px] cursor-pointer text-slate-600 opacity-80 hover:opacity-100" />
-        <div className="scroll flex snap-x snap-mandatory gap-1 overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide">
+        <div
+          ref={scrollRef}
+          className="scroll flex snap-x snap-mandatory gap-1 overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide"
+        >
           {Array.from({ length: visibleCells }).map((_, i) => {
             const element = state.data[i];
 
