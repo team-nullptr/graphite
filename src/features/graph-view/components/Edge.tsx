880aeeb57c05c740253eaada487a683193f62198
Bartosz Bieniek <itsbk20@gmail.com>
2023-09-17 16:37:47 +0200

    Display weights next to a straight edge

--- src/features/graph-view/components/Edge.tsx
+++ src/features/graph-view/components/Edge.tsx
@@ -1,8 +1,9 @@
+import { useRef } from "react";
+import colors from "tailwindcss/colors";
+import { cn } from "~/lib/utils";
 import type { Color } from "~/types/color";
 import { Vec2 } from "../types/vec2";
 import { Arrow } from "./Arrow";
-import colors from "tailwindcss/colors";
-import { cn } from "~/lib/utils";
 
 const vertexRadius = 19;
 
@@ -16,6 +17,7 @@ export type EdgeProps = {
   position?: number;
   circular?: boolean;
   thicken?: boolean;
+  weight?: number;
 };
 
 export function Edge(props: EdgeProps) {
@@ -32,7 +34,10 @@ function StraightEdge({
   color = "slate",
   thicken = false,
   position = 0,
+  weight,
 }: EdgeProps) {
+  const edgePathRef = useRef<SVGPathElement>(null);
+
   const width = dx - x;
   const height = dy - y;
   const length = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
@@ -53,17 +58,28 @@ function StraightEdge({
 
   const stroke = colors[color][400];
 
+  const pathElement = edgePathRef.current;
+  const edgeLabelPosition = pathElement ? getPathCenter(pathElement) : new Vec2(0, 0);
+
   return (
     <g transform={transform}>
       <path
+        ref={edgePathRef}
         className={cn("fill-none stroke-1 transition-[stroke]", thicken && "stroke-[3px]")}
         d={path}
         stroke={stroke}
       />
+      {weight && <EdgeLabel text={weight.toString()} position={edgeLabelPosition} angle={-deg} />}
       {directed && <Arrow position={end} angle={-angle} color={color} />}
     </g>
   );
 }
+
+const getPathCenter = (path: SVGPathElement) => {
+  const length = path.getTotalLength();
+  const centerPoint = path.getPointAtLength(length / 2);
+  return new Vec2(centerPoint.x, centerPoint.y);
+};
 
 const CircularEdge = ({ x, y, directed, position = 0, color = "slate" }: EdgeProps) => {
   const radius = 6 * position + 15;
@@ -73,7 +89,7 @@ const CircularEdge = ({ x, y, directed, position = 0, color = "slate" }: EdgePro
   // Intersection of two rounds - the vertex and the edge
   const arrowX = vertexRadius - radius ** 2 / (2 * vertexRadius);
   const arrowY = Math.sqrt(vertexRadius ** 2 - arrowX ** 2);
-  const arrowPostion: Vec2 = new Vec2(arrowX, arrowY);
+  const arrowPosition: Vec2 = new Vec2(arrowX, arrowY);
 
   // TODO: Replace 1.2 with actual calculations, to determine the right angle
   // This is due to how arrows are drawn (rotated)
@@ -91,7 +107,7 @@ const CircularEdge = ({ x, y, directed, position = 0, color = "slate" }: EdgePro
         r={radius}
         stroke={stroke}
       />
-      {directed && <Arrow position={arrowPostion} angle={arrowAngle} color={color} />}
+      {directed && <Arrow position={arrowPosition} angle={arrowAngle} color={color} />}
     </g>
   );
 };
@@ -109,4 +125,45 @@ const getLinePath = (
   const { x: bx, y: by } = b;
 
   return `M${sx} ${sy}C${ax} ${ay},${bx} ${by},${ex} ${ey}`;
+};
+
+interface EdgeTextProps {
+  text: string;
+  position: Vec2;
+  angle: number;
+}
+
+const EdgeLabel = (props: EdgeTextProps) => {
+  const labelTextRef = useRef<SVGTextElement>(null);
+
+  const { x, y } = props.position;
+  // TODO: Consider using this without a background: y + 5 * (props.angle > 0 ? -1 : 1);
+
+  const labelTextBBox = labelTextRef.current?.getBBox();
+  const labelTextWidth = labelTextBBox?.width ?? 0;
+  const labelTextHeight = labelTextBBox?.height ?? 0;
+
+  return (
+    <g transform={`rotate(${props.angle})`} transform-origin={`${x} ${y}`}>
+      <line
+        x1={x - labelTextWidth / 2}
+        y1={y}
+        x2={x + labelTextWidth / 2}
+        y2={y}
+        stroke="#e4e7eb"
+        strokeWidth={labelTextHeight}
+        strokeLinecap="round"
+      />
+      <text
+        ref={labelTextRef}
+        x={x}
+        y={y}
+        dominantBaseline="central"
+        textAnchor="middle"
+        fontSize={12}
+      >
+        {props.text}
+      </text>
+    </g>
+  );
 };
