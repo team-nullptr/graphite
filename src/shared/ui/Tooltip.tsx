86322b627b41e3778f3a82649b06a20a8bf08bcb
LordPrinz <plazinski2902@gmail.com>
2023-09-20 12:34:55 +0200

    Prevent tooltip flashing

--- src/shared/ui/Tooltip.tsx
+++ src/shared/ui/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { RefObject } from "react";
+import { type RefObject } from "react";
 
 export type TooltipProps = {
   label: string;
@@ -10,14 +10,14 @@ export function Tooltip({ elementRef, label }: TooltipProps) {
     return null;
   }
 
-  const { top, left, width } = elementRef.current.getBoundingClientRect();
+  const { top, left, height } = elementRef.current.getBoundingClientRect();
 
   return (
     <div
-      className="absolute z-[100] -translate-x-1/2 whitespace-nowrap rounded-[4px] bg-slate-800 px-3 py-2 text-slate-200"
+      className="absolute z-[100] -translate-y-0.5 whitespace-nowrap rounded-[4px] bg-slate-800 px-3 py-2 text-slate-200"
       style={{
-        top: top - 50,
-        left: left + width / 2,
+        top: top - height - 38,
+        left: left - 75.71875 - 37.86,
       }}
     >
       <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rounded-full bg-slate-800" />
